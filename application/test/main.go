f56ef14fcb335d86461473720d03ccc295b59991
TingerSure <tingersure@gmail.com>
2020-05-07 15:36:53 +0800

    update main

--- application/test/main.go
+++ application/test/main.go
@@ -17,7 +17,7 @@ const (
 func getVM() *runtime.Runtime {
 	VM := runtime.NewRuntime(&runtime.RuntimeParam{
 		OnError: func(err error) {
-			os.Stdout.WriteString(fmt.Sprintf("\033[1;35m[NL]: \033[00m%v\n", err.Error()))
+			os.Stdout.WriteString(fmt.Sprintf("\033[1;35m[NL]:\033[00m %v\n", err.Error()))
 		},
 		OnPrint: func(value concept.Variable) {
 			os.Stdout.WriteString(fmt.Sprintf("\033[1;36m[NL]:\033[00m %v\n", value.ToString("")))
@@ -27,7 +27,7 @@ func getVM() *runtime.Runtime {
 	VM.GetLibraryManager().AddSystemLibrary(system.NewSystemLibrary(VM.GetLibraryManager(), &system.SystemLibraryParam{
 		Std: &std.StdParam{
 			Error: func(value concept.Variable) {
-				os.Stdout.WriteString(fmt.Sprintf("\033[1;35m[NL]: \033[00m%v\n", value.ToString("")))
+				os.Stdout.WriteString(fmt.Sprintf("\033[1;35m[NL]:\033[00m %v\n", value.ToString("")))
 			},
 			Print: func(value concept.Variable) {
 				os.Stdout.WriteString(fmt.Sprintf("\033[1;36m[NL]:\033[00m %v\n", value.ToString("")))
@@ -45,7 +45,7 @@ func test4() {
 	VM := getVM()
 	err := VM.Start()
 	if err != nil {
-		os.Stdout.WriteString(fmt.Sprintf("\033[1;36m[System]: \033[00m%v\n", err.Error()))
+		os.Stdout.WriteString(fmt.Sprintf("\033[1;36m[System]:\033[00m %v\n", err.Error()))
 		return
 	}
 	cli := runtime.NewScan(&runtime.ScanParam{
@@ -53,17 +53,17 @@ func test4() {
 		OnReader: func(input string) bool {
 			index, err := VM.Deal(input)
 			if err != nil {
-				fmt.Printf("\033[1;32m[ERROR]: \033[00m%v\n", err.Error())
+				fmt.Printf("\033[1;32m[ERROR]:\033[00m %v\n", err.Error())
 				return true
 			}
 
-			fmt.Printf("\033[1;32m[LOG]: \033[00m%v\n", index.ToString(""))
+			fmt.Printf("\033[1;32m[LOG]:\033[00m %v\n", index.ToString(""))
 			VM.Exec(index)
 
 			return true
 		},
 		BeforeReader: func() {
-			os.Stderr.WriteString("\033[1;36m[TS]: \033[00m")
+			os.Stderr.WriteString("\033[1;36m[TS]:\033[00m ")
 		},
 	})
 	cli.Run()
