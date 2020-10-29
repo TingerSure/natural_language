74ae8396e36b46e20c911ddc6492741379fa9e4f
TingerSure <tingersure@gmail.com>
2020-10-29 17:59:16 +0800

    Update core/parser.Grammar.ParseStruct

--- core/parser/grammar.go
+++ core/parser/grammar.go
@@ -23,27 +23,32 @@ func (g *Grammar) ParseStruct(road *Road) error {
 			continue
 		}
 
-		origins := road.GetRightSection(index, nil)
-		targets := []tree.Phrase{}
+		originSources := road.GetRightSection(index, nil)
+		origins := map[tree.Phrase]bool{}
+		for _, origin := range originSources {
+			origins[origin] = true
+		}
+		targets := map[tree.Phrase]bool{}
+		activeTargets := map[tree.Phrase]bool{}
 		for len(origins) > 0 {
-			for _, origin := range origins {
+			for origin, _ := range origins {
 				rules := g.reach.GetRulesByLastType(origin.Types())
 				for _, rule := range rules {
-					targets = g.match(road, index, origin, rule, targets)
+					g.match(road, index, origin, rule, targets)
 				}
 			}
-			activeTargets := []tree.Phrase{}
-			for _, target := range targets {
+			for target, _ := range targets {
 				old := road.GetRightSectionByTypesAndSize(index, target.Types(), target.ContentSize())
 				if nl_interface.IsNil(old) {
 					road.AddRightSection(index, target)
-					activeTargets = append(activeTargets, target)
+					activeTargets[target] = true
 					continue
 				}
 				if priority, ok := old.(*tree.PhrasePriority); ok {
 					results, abandons := g.barricade.TargetFilter(priority.AllValues(), target)
 					g.cut(road, index, abandons)
 					if 1 == len(results) {
+						g.replace(targets, old, results[0])
 						road.ReplaceRight(index, old, results[0])
 					} else {
 						priority.SetValues(results)
@@ -54,25 +59,49 @@ func (g *Grammar) ParseStruct(road *Road) error {
 				g.cut(road, index, abandons)
 				switch results {
 				case 0:
-					road.ReplaceRight(index, old, tree.NewPhrasePriority([]tree.Phrase{
+					multiple := tree.NewPhrasePriority([]tree.Phrase{
 						old,
 						target,
-					}))
+					})
+					g.replace(targets, old, multiple)
+					road.ReplaceRight(index, old, multiple)
 					break
 				case -1:
 					// Do Nothing
 					break
 				case 1:
+					g.replace(targets, old, target)
 					road.ReplaceRight(index, old, target)
 					break
 				}
 			}
 			origins = activeTargets
-			targets = []tree.Phrase{}
-			activeTargets = []tree.Phrase{}
+			targets = map[tree.Phrase]bool{}
+			activeTargets = map[tree.Phrase]bool{}
 		}
 	}
 	return nil
+}
+
+func (g *Grammar) replace(targets map[tree.Phrase]bool, from, to tree.Phrase) {
+	replace := func(target tree.Phrase) {
+		if target == nil {
+			return
+		}
+		for index := 0; index < target.Size(); index++ {
+			if target.GetChild(index) == from {
+				target.SetChild(index, to)
+			}
+		}
+	}
+	for target, _ := range targets {
+		if priority, ok := target.(*tree.PhrasePriority); ok {
+			for index := 0; index < priority.ValueSize(); index++ {
+				replace(priority)
+			}
+		}
+		replace(target)
+	}
 }
 
 func (g *Grammar) cut(road *Road, index int, abandons *tree.AbandonGroup) {
@@ -86,28 +115,28 @@ func (g *Grammar) cut(road *Road, index int, abandons *tree.AbandonGroup) {
 	}
 }
 
-func (g *Grammar) match(road *Road, roadIndex int, last tree.Phrase, rule *tree.StructRule, back []tree.Phrase) []tree.Phrase {
+func (g *Grammar) match(road *Road, roadIndex int, last tree.Phrase, rule *tree.StructRule, back map[tree.Phrase]bool) {
 	size := rule.Size()
 	treasures := make([]tree.Phrase, size, size)
 	treasures[size-1] = last
 	if size == 1 {
-		return append(back, rule.Create(treasures))
+		back[rule.Create(treasures)] = true
+		return
 	}
-	return g.matchStep(road, size-2, roadIndex-last.ContentSize(), treasures, rule, back)
+	g.matchStep(road, size-2, roadIndex-last.ContentSize(), treasures, rule, back)
 }
 
-func (g *Grammar) matchStep(road *Road, index int, cursor int, treasures []tree.Phrase, rule *tree.StructRule, back []tree.Phrase) []tree.Phrase {
+func (g *Grammar) matchStep(road *Road, index int, cursor int, treasures []tree.Phrase, rule *tree.StructRule, back map[tree.Phrase]bool) {
 	if cursor < 0 {
-		return back
+		return
 	}
 	phrases := road.GetRightSectionByTypes(cursor, rule.Types()[index])
 	for phrase, _ := range phrases {
 		treasures[index] = phrase
 		if index == 0 {
-			back = append(back, rule.Create(treasures))
+			back[rule.Create(treasures)] = true
 		} else {
-			back = g.matchStep(road, index-1, cursor-phrase.ContentSize(), treasures, rule, back)
+			g.matchStep(road, index-1, cursor-phrase.ContentSize(), treasures, rule, back)
 		}
 	}
-	return back
 }
