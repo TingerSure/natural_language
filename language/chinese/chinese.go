d7cc5034cf1fc623a45519cba74c621f69e8dc78
TingerSure <tingersure@gmail.com>
2021-06-28 21:06:41 +0800

    Remove useless old language_bind libs.

--- language/chinese/chinese.go
+++ language/chinese/chinese.go
@@ -2,14 +2,9 @@ package chinese
 
 import (
 	"github.com/TingerSure/natural_language/core/tree"
-	"github.com/TingerSure/natural_language/language/chinese/bind_language"
 	"github.com/TingerSure/natural_language/language/chinese/system"
 )
 
 func BindRule(libs *tree.LibraryManager, chineseName string) {
 	system.BindRule(libs, chineseName)
-}
-
-func BindLanguage(libs *tree.LibraryManager, chineseName string) {
-	bind_language.BindLanguage(libs, chineseName)
 }
